b5ca6276836b993d7b31d4a200896b57f2dc16d2
Amaro "KuryKat" Alexandre <57152799+KuryKat@users.noreply.github.com>
2023-02-12 04:14:18 -0300

    Update Config Version +1

--- src/main/java/me/hypherionmc/sdlinklib/config/ConfigController.java
+++ src/main/java/me/hypherionmc/sdlinklib/config/ConfigController.java
@@ -40,7 +40,7 @@ public class ConfigController {
 
     // Private Variables used internally
     private final File configPath;
-    public static int configVer = 21;
+    public static int configVer = 22;
 
     // Instance of loaded config
     public static ModConfig modConfig;
